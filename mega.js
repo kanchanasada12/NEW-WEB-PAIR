825bf4af59cf16a8b50633957c61ed9412fa3295
kanchanasada12 <sadarukanchana5@gmail.com>
2025-08-05 09:48:16 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
   email: "sadarukanchana@5gmail.com",
-  password: "kanchanasadaru@12345",
+  password: "kanchana@2006",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,4 +41,5 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
 
